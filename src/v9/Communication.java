94faab713a1916b222d5728545356c7a7fb2eeb2
RyanAspen <ryanaspen27@gmail.com>
2024-01-10 22:07:31 -0500

    Added V9

--- src/v9/Communication.java
+++ src/v9/Communication.java
@@ -1,4 +1,4 @@
-package v7;
+package v9;
 
 import battlecode.common.FlagInfo;
 import battlecode.common.GameActionException;
@@ -15,14 +15,21 @@ class Communication {
     private static final int ATTACKER_ROLE_IDX = 7;
 
     private static final int DEFENDER_ROLE_IDX = 8;
-    private static final int BUILDER_ROLE_IDX = 9;
-    private static final int HEALER_ROLE_IDX = 10;
 
     private static int normalRoleCount = 0;
     private static int attackerRoleCount = 0;
     private static int defenderRoleCount = 0;
-    private static int builderRoleCount = 0;
-    private static int healerRoleCount = 0;
+
+    private static final int CLUSTER_POINT_IDX = 9;
+    private static final int NUM_CLUSTER_POINTS = 2;
+
+    private static final int MIN_DIST_BETWEEN_CLUSTERS = 50;
+
+    private static final int MAX_DEFENDERS_PER_FLAG = 4;
+    private static final int FLAG_DEFENSE_IDX = CLUSTER_POINT_IDX + NUM_CLUSTER_POINTS;
+    private static final int DISTRESS_IDX = FLAG_DEFENSE_IDX + 3;
+
+    private static MapLocation[] clusters = new MapLocation[0];
 
 
 
@@ -42,12 +49,6 @@ class Communication {
             case Defender:
                 offsetPrev = DEFENDER_ROLE_IDX;
                 break;
-            case Builder:
-                offsetPrev = BUILDER_ROLE_IDX;
-                break;
-            case Healer:
-                offsetPrev = HEALER_ROLE_IDX;
-                break;
             default:
                 offsetPrev = NORMAL_ROLE_IDX;
                 break;
@@ -59,12 +60,6 @@ class Communication {
                 break;
             case Defender:
                 offsetNew = DEFENDER_ROLE_IDX;
-                break;
-            case Builder:
-                offsetNew = BUILDER_ROLE_IDX;
-                break;
-            case Healer:
-                offsetNew = HEALER_ROLE_IDX;
                 break;
             default:
                 offsetNew = NORMAL_ROLE_IDX;
@@ -84,15 +79,90 @@ class Communication {
                 return normalRoleCount;
             case Attacker:
                 return attackerRoleCount;
-            case Builder:
-                return builderRoleCount;
             case Defender:
                 return defenderRoleCount;
-            case Healer:
-                return healerRoleCount;
             default:
                 return 0;
         }
+    }
+
+    public static MapLocation getDefenderPosition(RobotController rc) throws GameActionException {
+        int minDist = 9999;
+        MapLocation bestPos = null;
+        int bestIdx = -1;
+        for (int i = 0; i < 3; i++)
+        {
+            int amount = rc.readSharedArray(i + FLAG_DEFENSE_IDX);
+            if (amount >= MAX_DEFENDERS_PER_FLAG) {}
+            else
+            {
+                MapLocation defenderPos = intToLocation(rc, rc.readSharedArray(i));
+                if (defenderPos == null)
+                {
+                    continue;
+                }
+                int dist = defenderPos.distanceSquaredTo(rc.getLocation());
+                if (dist < minDist)
+                {
+                    minDist = dist;
+                    bestPos = defenderPos;
+                    bestIdx = i + FLAG_DEFENSE_IDX;
+                }
+            }
+        }
+        if (bestPos != null)
+        {
+            rc.writeSharedArray(bestIdx, rc.readSharedArray(bestIdx) + 1);
+        }
+        return bestPos;
+    }
+
+    public static void removeDefenderPosition(RobotController rc, MapLocation loc) throws GameActionException {
+        for (int i = 0; i < 3; i++)
+        {
+            MapLocation defenderPos = intToLocation(rc, rc.readSharedArray(i));
+            if (defenderPos != null && defenderPos.equals(loc))
+            {
+                int amount = rc.readSharedArray(i + FLAG_DEFENSE_IDX);
+                rc.writeSharedArray(i + FLAG_DEFENSE_IDX, amount - 1);
+            }
+        }
+    }
+
+    public static void sendDistressSignal(RobotController rc, MapLocation loc) throws GameActionException {
+        for (int i = 0; i < 3; i++)
+        {
+            MapLocation defenderPos = intToLocation(rc, rc.readSharedArray(i));
+            if (defenderPos != null && defenderPos.equals(loc))
+            {
+                rc.writeSharedArray(i +DISTRESS_IDX, 1);
+                break;
+            }
+        }
+    }
+
+    public static void removeDistressSignal(RobotController rc, MapLocation loc) throws GameActionException {
+        for (int i = 0; i < 3; i++)
+        {
+            MapLocation defenderPos = intToLocation(rc, rc.readSharedArray(i));
+            if (defenderPos != null && defenderPos.equals(loc))
+            {
+                rc.writeSharedArray(i +DISTRESS_IDX, 0);
+                break;
+            }
+        }
+    }
+
+    public static MapLocation getDistressLoc(RobotController rc) throws GameActionException {
+        for (int i = 0; i < 3; i++)
+        {
+            boolean distress = rc.readSharedArray(i+DISTRESS_IDX) > 0;
+            if (distress)
+            {
+                return intToLocation(rc, rc.readSharedArray(i));
+            }
+        }
+        return null;
     }
 
     private static void reportFlag(RobotController rc, FlagInfo flag) throws GameActionException {
@@ -178,12 +248,58 @@ class Communication {
         }
     }
 
+    public static boolean createClusterPoint(RobotController rc, MapLocation loc) throws GameActionException {
+        int idx = -1;
+        for (int i = CLUSTER_POINT_IDX; i < CLUSTER_POINT_IDX + NUM_CLUSTER_POINTS; i++)
+        {
+            MapLocation prevClusterLoc = intToLocation(rc, rc.readSharedArray(i));
+            if (prevClusterLoc == null)
+            {
+                idx = i;
+            }
+            else if (prevClusterLoc.isWithinDistanceSquared(loc, MIN_DIST_BETWEEN_CLUSTERS))
+            {
+                idx = i;
+                break;
+            }
+        }
+        if (idx != -1)
+        {
+            rc.writeSharedArray(idx, locationToInt(rc, loc));
+            return true;
+        }
+        return false;
+    }
+
+    public static void removeClusterPoint(RobotController rc, MapLocation loc) throws GameActionException {
+        for (int i = CLUSTER_POINT_IDX; i < CLUSTER_POINT_IDX + NUM_CLUSTER_POINTS; i++)
+        {
+            MapLocation prevClusterLoc = intToLocation(rc, rc.readSharedArray(i));
+            if (loc.equals(prevClusterLoc))
+            {
+                rc.writeSharedArray(i, locationToInt(rc, null));
+            }
+        }
+    }
+
+    private static void updateClusters(RobotController rc) throws GameActionException {
+        MapLocation[] clusterPoints = new MapLocation[NUM_CLUSTER_POINTS];
+        int j = 0;
+        for (int i = CLUSTER_POINT_IDX; i < CLUSTER_POINT_IDX + NUM_CLUSTER_POINTS; i++) {
+            MapLocation m = intToLocation(rc, rc.readSharedArray(i));
+            if (m != null) {
+                clusterPoints[j] = m;
+                j++;
+            }
+        }
+        clusters = new MapLocation[j];
+        System.arraycopy(clusterPoints, 0, clusters, 0, j);
+    }
+
     private static void updateRoleCounts(RobotController rc) throws GameActionException {
         normalRoleCount = rc.readSharedArray(NORMAL_ROLE_IDX);
         attackerRoleCount = rc.readSharedArray(ATTACKER_ROLE_IDX);
         defenderRoleCount = rc.readSharedArray(DEFENDER_ROLE_IDX);
-        builderRoleCount = rc.readSharedArray(BUILDER_ROLE_IDX);
-        healerRoleCount = rc.readSharedArray(HEALER_ROLE_IDX);
     }
     private static void updateFlags(RobotController rc) {
         MapLocation[] friendlyFlags = new MapLocation[3];
@@ -228,6 +344,11 @@ class Communication {
         return enemyFlagLocations;
     }
 
+    public static MapLocation[] getClusters()
+    {
+        return clusters;
+    }
+
     public static void updateComms(RobotController rc) throws GameActionException {
         FlagInfo[] flags = rc.senseNearbyFlags(-1);
         if (flags.length > 0)
@@ -247,6 +368,7 @@ class Communication {
         }
         updateFlags(rc);
         updateRoleCounts(rc);
+        updateClusters(rc);
         //If there are no flags close but comms says otherwise, remove from comms
         if (flags.length == 0)
         {
---
+++ src/v9/Strategy.java
@@ -0,0 +1,140 @@
+package v9;
+
+/*
+    This file is meant for macro-level strategy. For example, decide whether to specialize in a
+    particular area, whether to mainly be a defender or attacker, etc.
+ */
+
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+import battlecode.common.SkillType;
+
+enum Role {
+    Normal,
+    Attacker,
+    Defender
+}
+
+public class Strategy {
+
+    private static Role currentRole = Role.Normal;
+    private static int MIN_DEFENDERS = 6;
+    private static int MIN_ATTACKERS = 15;
+
+    private static void changeRole(RobotController rc, Role role) throws GameActionException {
+        if (role != currentRole)
+        {
+            Communication.reportRoleSwitch(rc, currentRole, role);
+            currentRole = role;
+
+        }
+    }
+
+    public static void changeStrategy(RobotController rc) throws GameActionException {
+        /*
+        if (rc.canBuyGlobal(GlobalUpgrade.ACTION))
+        {
+            rc.buyGlobal(GlobalUpgrade.ACTION);
+        }
+        if (rc.canBuyGlobal(GlobalUpgrade.CAPTURING))
+        {
+            rc.buyGlobal(GlobalUpgrade.CAPTURING);
+        }
+         */
+        MapLocation[] friendlyFlags = Communication.getFriendlyFlagLocations();
+        MapLocation[] enemyFlags = Communication.getEnemyFlagLocations();
+        if (friendlyFlags.length == 3)
+        {
+            //Offensive Strategy
+            MIN_DEFENDERS = 12;
+            MIN_ATTACKERS = 38;
+        }
+        else if (friendlyFlags.length > enemyFlags.length)
+        {
+            //Defensive Strategy (TURTLE)
+            MIN_DEFENDERS = 12;
+            MIN_ATTACKERS = 38;
+        }
+        else if (enemyFlags.length > friendlyFlags.length)
+        {
+            //Very Offensive Strategy
+            MIN_DEFENDERS = 12;
+            MIN_ATTACKERS = 38;
+        }
+    }
+
+    public static Role getRole(RobotController rc) throws GameActionException {
+        int attackExp = rc.getExperience(SkillType.ATTACK);
+        int buildExp = rc.getExperience(SkillType.BUILD);
+        int healExp = rc.getExperience(SkillType.HEAL);
+        int normalRoleCount = Communication.getRoleCount(rc, Role.Normal);
+        int attackerRoleCount = Communication.getRoleCount(rc, Role.Attacker);
+        int defenderRoleCount = Communication.getRoleCount(rc, Role.Defender);
+        if (rc.getID() == 10799)
+        {
+            System.out.println("Normal: " + normalRoleCount +
+                    " Attackers: " + attackerRoleCount +
+                    " Defenders: " + defenderRoleCount
+            );
+        }
+
+        boolean canSwitchRole;
+        switch (currentRole)
+        {
+            case Normal:
+                canSwitchRole = true;
+                break;
+            case Attacker:
+                canSwitchRole = (attackerRoleCount > MIN_ATTACKERS);
+                break;
+            case Defender:
+                canSwitchRole = (defenderRoleCount > MIN_DEFENDERS);
+                break;
+            default:
+                canSwitchRole = false;
+        }
+
+        //Only change role from a non-Normal role if global role counts are imbalanced
+        /*
+        if (currentRole != Role.Normal)
+        {
+            // Check if there are enough defenders
+            if (defenderRoleCount < MIN_DEFENDERS && currentRole != Role.Builder)
+            {
+                changeRole(rc, Role.Defender);
+            }
+            //Offense won't be strong enough unless we have enough attackers
+            else if (healerRoleCount >= attackerRoleCount)
+            {
+                changeRole(rc, Role.Attacker);
+            }
+            else if (builderRoleCount < MIN_BUILDERS)
+            {
+                changeRole(rc, Role.Builder);
+            }
+        }
+
+         */
+        //else
+        //{
+            if (rc.getRoundNum() > 150 && canSwitchRole)
+            {
+                if (defenderRoleCount < MIN_DEFENDERS)
+                {
+                    changeRole(rc, Role.Defender);
+                }
+                else if (attackerRoleCount < MIN_ATTACKERS)
+                {
+                    changeRole(rc, Role.Attacker);
+                }
+                else
+                {
+                    changeRole(rc, Role.Attacker);
+                }
+            }
+
+        //}
+        return currentRole;
+    }
+}
